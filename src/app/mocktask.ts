5083eb7e5dfafb34737780727f0a04895bd222a0
Lucille <82943831+luciadobre@users.noreply.github.com>
2023-06-15 18:21:16 +0300

    tasks

---
+++ src/app/mocktask.ts
@@ -0,0 +1,65 @@
+import {Task} from './Task'
+
+export const TASKS: Task[] = [
+    {
+      id: 1,
+      text: 'Buy groceries',
+      day: 'June 16th at 10:00am',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Finish project',
+      day: 'June 17th at 3:00pm',
+      reminder: false,
+    },
+    {
+      id: 3,
+      text: 'Call a friend',
+      day: 'June 18th at 11:30am',
+      reminder: true,
+    },
+    {
+      id: 4,
+      text: 'Go for a run',
+      day: 'June 19th at 8:00am',
+      reminder: true,
+    },
+    {
+      id: 5,
+      text: 'Read a book',
+      day: 'June 20th at 6:00pm',
+      reminder: false,
+    },
+    {
+      id: 6,
+      text: 'Attend a meeting',
+      day: 'June 21st at 2:00pm',
+      reminder: false,
+    },
+    {
+      id: 7,
+      text: 'Pay bills',
+      day: 'June 22nd at 9:30am',
+      reminder: true,
+    },
+    {
+      id: 8,
+      text: 'Clean the house',
+      day: 'June 23rd at 1:00pm',
+      reminder: true,
+    },
+    {
+      id: 9,
+      text: 'Watch a movie',
+      day: 'June 24th at 7:00pm',
+      reminder: false,
+    },
+    {
+      id: 10,
+      text: 'Take a nap',
+      day: 'June 25th at 4:30pm',
+      reminder: true,
+    },
+  ];
+  
